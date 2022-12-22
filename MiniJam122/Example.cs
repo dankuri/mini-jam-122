abfc51f9df3404e186dfb43bf9e3cc47d35e221f
GamerZero <WildlandHerald@mail.ru>
2022-12-23 02:51:50 +0300

    Restructure solution

---
+++ MiniJam122/Example.cs
@@ -0,0 +1,13 @@
+namespace MiniJam122;
+
+using System;
+using Common;
+using Godot;
+
+public class Example : Node
+{
+	public override void _Ready()
+	{
+		Console.WriteLine(Say.hello("C#"));
+	}
+}
